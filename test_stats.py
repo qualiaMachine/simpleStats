8583a4268cf5d9fbb4d7f0a1d3f13bccc1b619e7
Chris Endemann <endemann@wisc.edu>
2016-01-15 14:58:43 -0800

    add test_stats.py

--- test_stats.py
+++ test_stats.py
@@ -1,13 +1,19 @@
 from stats import mean
+from nose.tools import assert_equal, assert_almost_equals
 
 def test_mean():
-	assert mean([2,4]) == 3.0
-test_mean()
+	assert_equal(mean([2,4]), 3.0)
+#test_mean()
 
 def test_empty_list():
-	assert mean([]) == 0.0
-test_empty_list()
+	assert_equal(mean([]), 0.0)
+#test_empty_list()
 
 def test_float_mean():
-	assert mean([1,2]) == 1.5
-test_float_mean()
+	assert_almost_equals(mean([.5,.5,1]), .6666666666)
+#test_float_mean()
+
+def test_str_list_mean():
+	assert_equal(mean(['1','2','3']), 2.0)
+	
+	
