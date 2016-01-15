9a3bf8b0f2410f67435d045736281b2cde92587f
Chris Endemann <endemann@wisc.edu>
2016-01-15 13:50:08 -0800

    moving file

--- stats.py
+++ stats.py
@@ -6,13 +6,20 @@ def mean(vals):
 	
 	total = sum(vals)
 	length = len(vals)
-	return total/length
+	
+	if length == 0:
+		return 0.0
+	else:
+		return total/length
+
 
 def test_mean():
 	assert mean([2,4]) == 3.0
 test_mean()
 
+
 def test_empty_list():
 	assert mean([]) == 0.0
 test_empty_list()
+
 
