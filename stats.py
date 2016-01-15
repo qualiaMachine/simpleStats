95d854ee0d2c2c09e229bbcff45474bb7dbd85ee
Chris Endemann <endemann@wisc.edu>
2016-01-15 13:33:06 -0800

    test_empty_list added

--- stats.py
+++ stats.py
@@ -10,16 +10,9 @@ def mean(vals):
 
 def test_mean():
 	assert mean([2,4]) == 3.0
-
 test_mean()
-
-
 
 def test_empty_list():
 	assert mean([]) == 0.0
-	
+test_empty_list()
 
-
-	
-#print(mean([2,4]))
-#print(mean("hello"))
